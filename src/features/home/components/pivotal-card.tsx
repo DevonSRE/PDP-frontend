c1def61fad567c09f06b486ac2b50d981d629409
Yakubu Peter <devpeta34@gmail.com>
2025-09-07 12:26:17 +0100

    ui: pivotal card

--- src/features/home/components/pivotal-card.tsx
+++ src/features/home/components/pivotal-card.tsx
@@ -10,19 +10,21 @@ interface CardProps {
 const PivotalCard: React.FC<CardProps> = ({ icon, title, description, isMainCard }) => {
   // Conditional classes based on the 'isMainCard' prop
   const cardClasses = isMainCard
-    ? 'bg-gray-800 rounded-3xl p-8 text-white' // Main card: dark background, white text
+    ? 'bg-gray-800 rounded-3xl p-8 text-white justify-start items-start' // Main card: dark background, white text
     : 'bg-white rounded-2xl p-6 transition duration-300 ease-in-out hover:bg-teal-800 hover:shadow-xl text-black'; // Non-main card: white background, black text
 
   return (
     <div className={cardClasses}>
       {/* Conditionally render the icon for the smaller cards with a circular teal avatar */}
       {icon && (
-        <div className="mb-4 w-12 h-12 rounded-full bg-teal-500 flex items-center justify-center text-white">
+        <div className="mb-4 w-14 h-14 rounded-full bg-[#064C3C] flex items-center justify-center text-white">
           {icon}
         </div>
       )}
-      <h3 className="text-xl font-semibold font-poppins">{title}</h3>
-      <p className="mt-2 font-poppins">{description}</p>
+      <div className="mb-4 items-start justify-start">
+      <h3 className="text-xl font-bold font-poppins mt-2 items-start">{title}</h3>
+      <p className="mt-4 font-poppins font-light items-start">{description}</p>
+      </div>
     </div>
   );
 };
--- src/features/home/components/pivotal-section.tsx
+++ src/features/home/components/pivotal-section.tsx
@@ -1,41 +1,46 @@
 'use client';
+import { GraduationIcon } from '@/components/icon/graduation-icon';
 import PivotalCard from './pivotal-card';
 import {
-  Gavel,
+  GraduationCapIcon,
   BriefcaseBusiness,
   CheckCircle,
   Users,
   GraduationCap,
   MapPin,
 } from 'lucide-react';
+import { BadgeIcon } from '@/components/icon/badge-icon';
+import { FaqIcon } from '@/components/icon/faq-icon';
+import { PanIcon } from '@/components/icon/pan-icon';
+import { TopTierIcon } from '@/components/icon/top-tier';
 
 const cardData = [
   {
-    icon: <Gavel size={32} />,
+    icon: <GraduationIcon/>,
     title: 'Practical, Context-Specific Learning',
     description:
       'Our courses are built around real-world scenarios and local challenges, not just theory. Participants leave with actionable skills they can use immediately.',
   },
   {
-    icon: <BriefcaseBusiness size={32} />,
+    icon: <BadgeIcon/>,
     title: 'Custom-Built for Every Client',
     description:
       'We don’t do one-size-fits-all. Each program is tailored after a clear understanding of your context, training needs, and strategic goals.',
   },
   {
-    icon: <CheckCircle size={32} />,
+    icon: <FaqIcon/>,
     title: 'End-to-End Support',
     description:
       'From curriculum design and skills gap analysis to delivery and post-training follow-up, we support you through the entire learning journey.',
   },
   {
-    icon: <MapPin size={32} />,
+    icon: <PanIcon/>,
     title: 'Pan-African & International Reach',
     description:
       "From West to East Africa and beyond, we've supported both public and private sector institutions with high-quality capacity building interventions.",
   },
   {
-    icon: <GraduationCap size={32} />,
+    icon: <TopTierIcon/>,
     title: 'Top-Tier Faculty & Practitioners',
     description:
       'Our facilitators are not just trainers—they are seasoned professionals with deep expertise in governance, leadership, procurement, and organizational development.',
@@ -47,7 +52,7 @@ const PivotalSection = () => {
     <div className="bg-[#F5F5F5] py-36 px-4 text-center font-poppins">
       {/* Main Heading and First Card */}
       <h2 className="text-3xl font-bold text-black">
-        Why <span className="text-teal-400">Choose Pivotal?</span>
+        Why <span className="text-[#009981]">Choose Pivotal?</span>
       </h2>
       <p className="mt-2 text-black max-w-2xl mx-auto">
         At Pivotal Development Partners, we do more than deliver training — we co-create learning experiences that drive results.
